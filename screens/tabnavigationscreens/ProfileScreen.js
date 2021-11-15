54a24e8b9479c0ab5c7511f3292ecbce6c8507ce
dobaybalazs <dobaybalazs@gmail.com>
2021-11-15 21:52:00 +0100

    Finish designv2

--- screens/tabnavigationscreens/ProfileScreen.js
+++ screens/tabnavigationscreens/ProfileScreen.js
@@ -1,10 +1,46 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View, Image, TouchableOpacity } from "react-native";
+
+import Colors from "../../constants/Colors";
+import Sizes from "../../constants/Sizes";
+import DefaultText from "../../components/texts/DefaultText";
 
 const ProfileScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>ProfileScreen</Text>
+      <View style={styles.header}>
+        <View style={styles.imageContainer}>
+          <Image
+            style={styles.image}
+            source={{
+              uri: "https://www.whatsappprofiledpimages.com/wp-content/uploads/2021/08/Profile-Photo-Wallpaper.jpg",
+            }}
+          />
+        </View>
+        <View>
+          <DefaultText style={{ color: Colors.whitecolor, fontSize: 20 }}>
+            Felhasználónév
+          </DefaultText>
+        </View>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.field}>
+          <DefaultText style={styles.text}>Név</DefaultText>
+        </View>
+        <View style={styles.field}>
+          <DefaultText style={styles.text}>Életkor</DefaultText>
+        </View>
+        <View style={styles.field}>
+          <DefaultText style={styles.text}>Nem</DefaultText>
+        </View>
+        <TouchableOpacity activeOpacity={Sizes.activeopacity}>
+          <View style={styles.Button}>
+            <DefaultText style={styles.ButtonText}>
+              Profil szerkesztése
+            </DefaultText>
+          </View>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -14,8 +50,59 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: Colors.bluecolor,
     alignItems: "center",
     justifyContent: "center",
+  },
+  imageContainer: {
+    elevation: 3,
+    width: 110,
+    height: 110,
+    borderRadius: 100,
+    overflow: "hidden",
+  },
+  image: {
+    width: 120,
+    height: 120,
+  },
+  header: {
+    flex: 3,
+    width: "100%",
+    backgroundColor: Colors.bluecolor,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    paddingBottom: 15,
+  },
+  body: {
+    flex: 7,
+    width: "100%",
+    backgroundColor: "white",
+    borderTopRightRadius: 35,
+    borderTopLeftRadius: 35,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  field: {
+    marginVertical: 10,
+    borderBottomColor: Colors.primarygray,
+    borderBottomWidth: 1,
+    width: "75%",
+    paddingBottom: 5,
+  },
+  text: {
+    fontSize: 20,
+    color: Colors.secondarygray,
+  },
+  Button: {
+    width: 300,
+    backgroundColor: Colors.bluecolor,
+    borderRadius: 40,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+  },
+  ButtonText: {
+    fontSize: Sizes.titlefontsize,
+    color: Colors.whitecolor,
   },
 });
--- screens/tabnavigationscreens/StatisticsScreen.js
+++ screens/tabnavigationscreens/StatisticsScreen.js
@@ -1,10 +1,196 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import SVG, { G, Circle, Text } from "react-native-svg";
+import Colors from "../../constants/Colors";
+
+import DefaultText from "../../components/texts/DefaultText";
 
 const StatisticsScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>StatisticsScreen</Text>
+      <View style={styles.header}>
+        <DefaultText style={{ fontSize: 24 }}>Statisztika</DefaultText>
+      </View>
+      <View style={styles.chart}>
+        <SVG width={250} height={250} viewBox={`0 0 100 100`}>
+          <G>
+            <Circle
+              cx="50%"
+              cy="50%"
+              stroke={Colors.redcolor}
+              strokeWidth={6}
+              r={40}
+              strokeOpacity={1.0}
+            />
+
+            <Circle
+              cx="50%"
+              cy="50%"
+              stroke="yellow"
+              strokeWidth={6}
+              r={40}
+              strokeOpacity={1.0}
+              strokeDasharray={2 * Math.PI * 40}
+              strokeDashoffset={(2 * Math.PI * 40) / 3}
+              strokeLinecap="square"
+            />
+            <Circle
+              cx="50%"
+              cy="50%"
+              stroke={Colors.bluecolor}
+              strokeWidth={6}
+              r={40}
+              strokeOpacity={1.0}
+              strokeDasharray={2 * Math.PI * 40}
+              strokeDashoffset={(2 * Math.PI * 40) / 2}
+              strokeLinecap="square"
+            />
+            <Circle
+              cx="50%"
+              cy="50%"
+              stroke={Colors.greencolor}
+              strokeWidth={6}
+              r={40}
+              strokeOpacity={1.0}
+              strokeDasharray={2 * Math.PI * 40}
+              strokeDashoffset={(3 * Math.PI * 40) / 2}
+              strokeLinecap="square"
+            />
+            <Text
+              x="38%"
+              y="45%"
+              text-anchor="middle"
+              stroke="none"
+              dy=".1em"
+              fontSize={14}
+              alignment-baseline="middle"
+              fill={Colors.secondarygray}
+            >
+              150
+            </Text>
+            <Text
+              x="28%"
+              y="55%"
+              text-anchor="middle"
+              stroke="none"
+              fill={Colors.secondarygray}
+              stroke-width="2px"
+              dy=".1em"
+              fontSize={8}
+              alignment-baseline="middle"
+            >
+              Megvásárolt
+            </Text>
+            <Text
+              x="38%"
+              y="65%"
+              text-anchor="middle"
+              stroke="none"
+              fill={Colors.secondarygray}
+              stroke-width="2px"
+              dy=".1em"
+              fontSize={8}
+              alignment-baseline="middle"
+            >
+              termék
+            </Text>
+          </G>
+        </SVG>
+      </View>
+      <View style={styles.body}>
+        <View style={styles.categoryContainer}>
+          <View
+            style={{
+              width: 8,
+              height: 8,
+              borderRadius: 10000,
+              backgroundColor: "red",
+            }}
+          ></View>
+          <View style={styles.category}>
+            <DefaultText style={styles.text}>Hús</DefaultText>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: 80,
+              }}
+            >
+              <DefaultText style={styles.text}>84</DefaultText>
+              <DefaultText style={styles.text}>42%</DefaultText>
+            </View>
+          </View>
+        </View>
+        <View style={styles.categoryContainer}>
+          <View
+            style={{
+              width: 8,
+              height: 8,
+              borderRadius: 10000,
+              backgroundColor: "green",
+            }}
+          ></View>
+          <View style={styles.category}>
+            <DefaultText style={styles.text}>Tejtermék</DefaultText>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: 80,
+              }}
+            >
+              <DefaultText style={styles.text}>32</DefaultText>
+              <DefaultText style={styles.text}>16%</DefaultText>
+            </View>
+          </View>
+        </View>
+        <View style={styles.categoryContainer}>
+          <View
+            style={{
+              width: 8,
+              height: 8,
+              borderRadius: 10000,
+              backgroundColor: "yellow",
+            }}
+          ></View>
+          <View style={styles.category}>
+            <DefaultText style={styles.text}>Gyümölcs</DefaultText>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: 80,
+              }}
+            >
+              <DefaultText style={styles.text}>54</DefaultText>
+              <DefaultText style={styles.text}>27%</DefaultText>
+            </View>
+          </View>
+        </View>
+        <View style={styles.categoryContainer}>
+          <View
+            style={{
+              width: 8,
+              height: 8,
+              borderRadius: 10000,
+              backgroundColor: Colors.bluecolor,
+            }}
+          ></View>
+          <View style={styles.category}>
+            <DefaultText style={styles.text}>Zöldség</DefaultText>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                width: 80,
+              }}
+            >
+              <DefaultText style={styles.text}>30</DefaultText>
+              <DefaultText style={styles.text}>15%</DefaultText>
+            </View>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
@@ -13,8 +199,39 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     backgroundColor: "#fff",
+  },
+  header: {
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  chart: {
+    flex: 2,
+    width: "100%",
+    alignItems: "center",
+  },
+  body: {
+    height: 250,
+    width: "100%",
+    alignItems: "center",
+  },
+  categoryContainer: {
+    width: "80%",
+    borderBottomWidth: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 5,
+  },
+  category: {
+    paddingVertical: 5,
+    paddingLeft: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "95%",
+  },
+  text: {
+    fontSize: 18,
+    color: Colors.secondarygray,
   },
 });
 
